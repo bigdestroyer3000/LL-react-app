b3ddfb36c0c2e83cdf31074a3cb97c25717df01a
nayavdoschak <nayavdoschak@nvutc.rosatomtech.ru>
2025-08-13 14:44:59 +0300

    3.4 - trpc all

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-export const App = () => {
-  const ideas = [
-    { nick: 'cool-idea-nick-1', name: 'idea 1', description: 'Description of idea 1...' },
-    { nick: 'cool-idea-nick-2', name: 'idea 2', description: 'Description of idea 2...' },
-    { nick: 'cool-idea-nick-3', name: 'idea 3', description: 'Description of idea 3...' },
-    { nick: 'cool-idea-nick-4', name: 'idea 4', description: 'Description of idea 4...' },
-    { nick: 'cool-idea-nick-5', name: 'idea 5', description: 'Description of idea 5...' },
-  ];
+import { TrpcProvider } from './lib/trps.tsx';
+import { AllIdeasPage } from './pages/Ideas';
 
+export const App = () => {
   return (
-    <div>
-      <h1>Ideanick</h1>
-      <div>
-        {ideas.map((idea) => {
-          return (
-            <div key={idea.nick}>
-              <h2>{idea.name}</h2>
-              <p>{idea.description}</p>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
+    <TrpcProvider>
+      <AllIdeasPage />
+    </TrpcProvider>
+  )
+}
